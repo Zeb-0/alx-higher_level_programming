e599e43f59dff417339e3edc2fe3afbc6bb67de9
Zeb-0 <morandezeb@gmail.com>
2023-03-07 02:20:04 -0800

    3-print_alphabt.py

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+#print alphabets in lower using ASCII except q & e
+for letter in range(97, 123):
+    if chr(letter) != 'q' and chr(letter) != 'e':
+        print("{}".format(chr(letter)), end="")
