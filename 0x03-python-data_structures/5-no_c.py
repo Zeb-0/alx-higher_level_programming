b2d3dec54e467f6c8616830b22b402befc5d6534
Zeb-0 <morandezeb@gmail.com>
2023-03-12 13:28:10 +0000

    5-no_c.py

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+'''  Remove all characters c and C from a string.'''
+
+
+def no_c(my_string):
+    copy = [x for x in my_string if x != 'c' and x != 'C']
+    return ("".join(copy))
