da2cd9d209bbfecf2bd0b7c42508799cf7c2dade
Zeberio <115374580+Zeb-0@users.noreply.github.com>
2023-03-27 16:57:31 +0300

    3-safe_print_division.py

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -6,6 +6,6 @@ def safe_print_division(a, b):
     except (ZeroDivisionError, TypeError):
         div = None
     finally:
-        print("{:d} / {:d} = {:d}".format(a, b, div))
-    return div
+        print("Inside result: {}".format(div))
+    return (div)
 
