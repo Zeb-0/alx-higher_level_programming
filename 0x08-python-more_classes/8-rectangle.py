c3517438cb494568b7465bd4a251ac4de1184ea4
Zeb-0 <morandezeb@gmail.com>
2023-04-03 11:27:02 -0700

    8-rectangle.py corrections

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -94,12 +94,12 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return ""
         else:
-            rectangle = ""
-            for i in range(self.__height):
-                for j in range(self.__width):
-                    rectangle += "#"
-                rectangle += "\n"
-            return rectangle[:-1]
+            rect = []
+        for i in range(self.__height):
+            [rect.append(str(self.print_symbol)) for j in range(self.__width)]
+            if i != self.__height - 1:
+                rect.append("\n")
+        return ("".join(rect))
 
     def __repr__(self):
         '''return string rep of rectangle'''
