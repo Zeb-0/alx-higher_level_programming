9473b18c10c0be7d622770b2236f0d887df13a6d
Zeb-0 <morandezeb@gmail.com>
2023-04-11 18:08:51 +0300

    adds a new attribute to an object if possible

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -3,11 +3,8 @@
 ''' adds a new attribute to an object if it’s possible: '''
 
 
-class Myclass:
-    '''creates a class Myclass '''
-    pass
-
-''' create instance of Myclass'''
-obj = MyClass()
-add_attribute(obj, 'new_attribute', 42)
-print(obj.new_attribute)  # prints 42
+def add_attributes(obj, attrib, value):
+    ''' try adding an attribute to an object '''
+    if not hasattr(obj, "__dict__"):
+        raise TypeError("not possible")
+    obj.__dict__[att] = value
