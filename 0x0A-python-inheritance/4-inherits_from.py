6509d09fbecb00c1576d1c358e0c770765e4a113
Zeb-0 <morandezeb@gmail.com>
2023-04-11 04:50:36 -0700

    Only is subclass

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 
-''' object instance of a class that inherited (directlyorindirectly) from class'''
+'''
+object instance of a class that inherited
+ (directlyorindirectly) from class
+ '''
 
 
 def inherits_from(obj, a_class):
-    ''' 
+    '''
     Return True if object is inctance f a class that inherited
     (directly or indirectly)  the specified class; otherwise False.
     '''
