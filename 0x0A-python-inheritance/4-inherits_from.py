79241e942437a7e9e1ae6af35104f93a9b1f4cab
Zeb-0 <morandezeb@gmail.com>
2023-04-11 14:46:13 +0300

    Only a subclass

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -6,9 +6,9 @@
 def inherits_from(obj, a_class):
     ''' 
     Return True if object is inctance f a class that inherited
-    (directly or indirectly) from the specified class; otherwise False.
+    (directly or indirectly)  the specified class; otherwise False.
     '''
-    if issubclass(type(obj), a_class)0 and type(obj) != a_class:
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
         return True
     else:
         return False
