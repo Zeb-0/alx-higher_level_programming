99c845192d429f2d7177a2ea090e0d79f008dbde
Zeb-0 <morandezeb@gmail.com>
2023-04-11 12:33:01 +0300

    sortd list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+''' Defines Mylist class inheritor '''
+
+
+class MyList(list):
+    '''
+    subclass of list
+    '''
+    def print_sorted(self):
+        '''prints list in ascending order
+        '''
+        sorted_list = sorted(self)
+        pint(sorted_list)
