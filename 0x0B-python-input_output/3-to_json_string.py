6c1cf9e6bbfa11199d3339c6ae7d5eccd6cf4c2e
Zeb-0 <morandezeb@gmail.com>
2023-04-12 18:24:48 +0300

    json

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+''' returns the JSON representation of an object (string): '''
+
+
+def to_json_string(my_obj):
+    ''' returns json rep of string object '''
+    return json.dumps(my_obj)
