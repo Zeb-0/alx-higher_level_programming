e09f0fa4878266d087e5b6bd9fe92b53c71a57e0
Zeberio <115374580+Zeb-0@users.noreply.github.com>
2023-07-04 10:16:42 +0300

    1-rectangle.py corrections.

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -16,15 +16,15 @@ class Rectangle:
             TypeError: if the size is not of type integer
         '''
 
-        self.width = width
-        self.height = height
+        self._width = width  # Use single underscore instead of double underscore
+        self._height = height  # Use single underscore instead of double underscore
 
     @property
     def width(self):
         '''
         retrieve the width attribute
         '''
-        return self.__width
+        return self._width  # Use single underscore instead of double underscore
 
     @width.setter
     def width(self, value):
@@ -33,19 +33,19 @@ class Rectangle:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-        self.__width = value
+        self._width = value  # Use single underscore instead of double underscore
 
-        @property
-        def height(self):
-            ''' method to retrieve the height attribute '''
-            return self.__height
+    @property
+    def height(self):
+        ''' method to retrieve the height attribute '''
+        return self._height  # Use single underscore instead of double underscore
 
-        @height.setter
-        def height(self, value):
-            ''' set the rectangle height attribute '''
+    @height.setter
+    def height(self, value):
+        ''' set the rectangle height attribute '''
 
-            if not isinstance(value, int):
-                raise TypeError("height must be an integer")
-            if value < 0:
-                raise ValueError("height must be >= 0")
-            self.__height = value
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self._height = value  # Use single underscore instead of double underscore
