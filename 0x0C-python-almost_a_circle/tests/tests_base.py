27e45092cf3e82c2f0f106a5bec9c99a7c487fb6
Zeb-0 <morandezeb@gmail.com>
2023-07-17 07:57:53 +0300

    unittests

---
+++ 0x0C-python-almost_a_circle/tests/tests_base.py
@@ -0,0 +1,21 @@
+# #!/usr/bin/python3
+
+# ''' unit tests for the base class
+
+# unittest classes:
+#     TestBase_instantiation - line 21
+# '''
+
+# import os
+# import unittest
+# from models.base import Base
+# from models.rectangle import Rectangle
+# from models.square import Square
+
+# class TestBase_instantiation(unittest.TestCase):
+#     ''' testing Base class instantiation.'''
+
+#     def test_no_arg(self):
+#         b1 = Base()
+#         b2 = Base()
+#         self.assertEqual(b1.id, b2.id - 1)
