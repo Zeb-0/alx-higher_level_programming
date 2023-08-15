221701803f2729843dd6e3ed2cd092ddcebf4f21
Zeb-0 <morandezeb@gmail.com>
2023-08-15 09:31:18 -0700

    list rows in the DB

---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,5 @@
+-- a script that lists all rows of the table first_table from the database hbtn_0c_0
+-- should print all fields
+-- pass Db name as argument of `mysql` command
+
+SELECT * FROM first_table;
