e9e8cd28e6a904250a9f74648db3222c65c2cd9e
Zeberio <morandezeb@gmail.com>
2023-09-11 08:19:22 -0700

    argument count

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+// check the number of args passed
+if (process.argv.length === 2) {
+	console.log('No argument');
+}
+else if (process.argv.length === 3) {
+	console.log('Argument found');
+}
+else {
+	console.log('Arguments found');
+}
