f079765f44e30a2978360bfdd58ec1ed9d832b0d
Zeberio <morandezeb@gmail.com>
2023-09-11 09:33:35 -0700

    executes x times a function

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,8 +1,4 @@
 #!/usr/bin/node
-const executeXTimes = (x, theFunction) => {
-  for (let i = 0; i < x; i++) {
-    theFunction();
-  }
+exports.callMeMoby = function (x, theFunction) {
+  for (let i = 0; i < x; i++) theFunction();
 };
-
-module.exports = executeXTimes;
