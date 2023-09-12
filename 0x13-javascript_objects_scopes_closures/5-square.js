43e74959489f564cce2ae62e9fb7c4ddf8ab4efc
Zeberio <morandezeb@gmail.com>
2023-09-12 07:54:35 -0700

    Class Square Inheritor

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/**
+ * Class square that inherits from 4-rectangle.js
+ */
+const Rectangle = require('.4-rectangle');
+
+class Square extends Rectangle {
+    constructor (size) {
+        super(size, size);
+    }
+}
+
+module.exports = Square;
