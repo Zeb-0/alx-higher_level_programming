c0586d56bf42798242dd8cb443baf0adda7006dd
Zeberio <morandezeb@gmail.com>
2023-09-12 06:49:20 -0700

    handle invalid rectangle dimensions

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+/**
+ * Rectangle handle incorrect dimensions
+ */
+class Rectangle {
+  constructor(w, h) {
+    if (w > 0 && h > 0 && Number.isInteger(w) && Number.isInteger(h)) {
+      this.width = w;
+      this.height = h;
+    } else {
+      // Create an empty object if w or h is not a positive integer or equal to 0
+      this.width = undefined;
+      this.height = undefined;
+    }
+  }
+}
