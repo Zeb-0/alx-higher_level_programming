0ad403b5f0ce80d8e5ab20ac17808adac539a26e
Zeberio <morandezeb@gmail.com>
2023-09-12 07:46:16 -0700

    check for parameters

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,16 +1,12 @@
 #!/usr/bin/node
 /**
- * Rectangle handle incorrect dimensions
+ * handle incorrect dimensions
  */
 class Rectangle {
-  constructor(w, h) {
-    if (w > 0 && h > 0 && Number.isInteger(w) && Number.isInteger(h)) {
+  constructor (w, h) {
+    if (typeof w === 'number' && w > 0 && typeof h === 'number' && h > 0) {
       this.width = w;
       this.height = h;
-    } else {
-      // Create an empty object if w or h is not a positive integer or equal to 0
-      this.width = undefined;
-      this.height = undefined;
     }
   }
 }
