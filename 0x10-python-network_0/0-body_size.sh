f9b650b602ab1c87d988f87dc8846eb506ab7ac7
Zeberio Morande <morandezeb@gmail.com>
2023-11-30 01:21:08 -0800

    send a request to a URL, display response

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-# send a request to a URL, display the size of the body of the response
+# send a request to
+# a URL, display the size of the body of the response
 curl -s "$1" | wc -c
